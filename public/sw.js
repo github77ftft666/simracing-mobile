547cd058be50292e9d6acab3ae68bc3a5af2e0a4
Artem Aleksandrovich <tema2018tim@gmail.com>
2026-08-31 18:53:10 +0300

    Refresh PWA cache for sensor readouts

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pitlink-v2'
+const CACHE = 'pitlink-v3'
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest']
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL))))
