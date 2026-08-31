dfbe9e178d9f79e0078365ce7a46b711eb46742c
Artem Aleksandrovich <tema2018tim@gmail.com>
2026-08-31 18:58:08 +0300

    Refresh PWA cache for control mapping

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pitlink-v3'
+const CACHE = 'pitlink-v4'
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest']
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL))))
